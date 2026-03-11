c659881e0bd079cd281975401b88d1dfa505858b
julia :) <juliavaleriogs@gmail.com>
2026-03-11 09:09:23 -0300

    criando .json, if/else e max 100 caracteres no card

--- 2-semestre/aula-05-06/aula-tailwind/src/pages/Home/Home.tsx
+++ 2-semestre/aula-05-06/aula-tailwind/src/pages/Home/Home.tsx
@@ -1,56 +1,25 @@
 import { Link } from "react-router-dom";
+import produtos from "../../data/produtos.json";
 
 import Card from "../../components/Card/Card";
 
 const Home = () => {
-  const produtos = [
-    {
-      id: 1,
-      nome: "Produto Garrafa Termica",
-      descricao: "Deixe seu café quentinho",
-    },
-    {
-      id: 2,
-      nome: "Produto 2",
-      descricao:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quidem deserunt, quo soluta animi nobis ducimus possimus? Quia temporibus consequuntur, obcaecati maxime recusandae enim sequi nulla a, fuga, totam tempora.",
-    },
-    {
-      id: 3,
-      nome: "Produto 3",
-      descricao:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quidem deserunt, quo soluta animi nobis ducimus possimus? Quia temporibus consequuntur, obcaecati maxime recusandae enim sequi nulla a, fuga, totam tempora.",
-    },
-    {
-      id: 4,
-      nome: "Produto 4",
-      descricao:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quidem deserunt, quo soluta animi nobis ducimus possimus? Quia temporibus consequuntur, obcaecati maxime recusandae enim sequi nulla a, fuga, totam tempora.",
-    },
-    {
-      id: 5,
-      nome: "Produto 5",
-      descricao:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quidem deserunt, quo soluta animi nobis ducimus possimus? Quia temporibus consequuntur, obcaecati maxime recusandae enim sequi nulla a, fuga, totam tempora.",
-    },
-    {
-      id: 6,
-      nome: "Produto Panela de Pressão",
-      descricao: "Panela de pressão anti-aderente",
-    },
-  ];
 
   return (
     <>
       <main className="flex-1 container mx-auto p-6">
         <div className="grid grid-cols-3 gap-6">
-
-
-
           {produtos.map((produto) => (
             <Card key={produto.id}>
-              <h2 className="text-2xl font-semibold mb-2">Produto {produto.id}</h2>
-              <p>{produto.descricao}</p>
+              <h2 className="text-2xl font-semibold mb-2">
+                {produto.nome}
+              </h2>
+              <p>
+                {produto.descricao.substring(0, 99) + "..."}
+                <Link to={`/produto/${produto.id}`} className="text-blue-500">
+                  Saiba mais
+                </Link>
+              </p>
               <Link
                 to={`/produto/${produto.id}`}
                 className="bg-green-500 text-white text-center px-4 py-2 rounded mt-6 inline-block"
@@ -59,10 +28,6 @@ const Home = () => {
               </Link>
             </Card>
           ))}
-
-
-
-
         </div>
       </main>
     </>
--- 2-semestre/aula-05-06/aula-tailwind/src/pages/ProdutoDetalhe/ProdutoDetalhe.tsx
+++ 2-semestre/aula-05-06/aula-tailwind/src/pages/ProdutoDetalhe/ProdutoDetalhe.tsx
@@ -1,15 +1,42 @@
 import { useParams } from "react-router-dom";
+import produtos from "../../data/produtos.json";
 
 const ProdutoDetalhe = () => {
-    const {id} = useParams(); //Aqui pegamos o id do produto que foi passado na URL
-  return (
-    <>
-      <main className="flex-1 container mx-auto p-6">
-        <div className="grid grid-cols-1 gap-6">
-          <h2 className="text-2xl font-semibold mb-2">Produto {id}</h2>
-        </div>
-      </main>
-    </>
-  );
+
+  const { id } = useParams(); //Aqui pegamos o id do produto que foi passado na URL
+
+  //BUSCA POR PRODUTO
+  const produto = produtos.find((p) => p.id === Number(id)); //Aqui buscamos o produto com o id correspondente
+
+  //SE NÃO ENCONTRADO, MOSTRA MENSAGEM DE ERRO
+  if (!produto) {
+    return (
+      <>
+        <main className="flex-1 container mx-auto p-6">
+          <div className="grid grid-cols-1 gap-6">
+            <h2 className="text-2xl font-semibold mb-2">
+              Produto não encontrado.
+            </h2>
+          </div>
+        </main>
+      </>
+    );
+  }
+
+  //SE TIVER ENCONTRADO, MOSTRA OS DETALHES DO PRODUTO
+  else {
+    return (
+      <>
+        <main className="flex-1 container mx-auto p-6">
+          <div className="grid grid-cols-1 gap-6">
+            <h2 className="text-2xl font-semibold mb-2">
+              {produto.nome}
+            </h2>
+            <p className="font-light text-2xl">{produto.descricao}</p>
+          </div>
+        </main>
+      </>
+    );
+  }
 };
 export default ProdutoDetalhe;
